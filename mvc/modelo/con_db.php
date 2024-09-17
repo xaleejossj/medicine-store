d131dd753c75c307f85e7b4f85f42b9879ff56ce
xaleejossj <junioralejandro2517@gmail.com>
2024-09-17 07:02:13 -0500

    Correccion conexion

--- mvc/modelo/con_db.php
+++ mvc/modelo/con_db.php
@@ -1,11 +1,11 @@
 <?php
 // Archivo de conexión a la base de datos
 $servername = "u964289722_medicine_store";
-$username = "u964289722_root";
+$username = "u964289722_medicine_store";
 $password = "Admin2024@";
 $database = "u964289722_medicine_store";
 
-$conn= new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
