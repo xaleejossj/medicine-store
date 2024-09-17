d6178c328c5ed17ddf1a4b5a24127a995d9cb5c3
xaleejossj <junioralejandro2517@gmail.com>
2024-09-17 12:34:26 -0500

    recover pass2

--- mvc/vista/recover_pass2.php
+++ mvc/vista/recover_pass2.php
@@ -8,38 +8,43 @@
     <title>Recuperar Contraseña</title>
 </head>
 <body>
+
     <div id="img-container">
         <img src="img/logo.png" width="125" height="120">
     </div>
-    <div class="main">  
+    <div class="main">
         <input type="checkbox" id="chk" aria-hidden="true">
+
         <div class="signup">
             <form action="recuperarClaveConLink.php" method="post">
                 <label for="chk" aria-hidden="true">Recuperar Contraseña</label>
-                <p id="text">Ingrese su correo electrónico</p>
+                <label for="chk" aria-hidden="true" id="text">Ingrese su correo electrónico</label>
                 <input type="email" name="email" placeholder="correo" required autocomplete="off"/>
                 <button type="submit">Enviar</button>
-				<br>
+                <br>
 
-                <!-- Mensaje de error de correo no encontrado -->
                 <?php if (isset($_REQUEST['errorEmail'])): ?>
                     <div class="alert alert-danger" role="alert">
-                        El correo no existe, intente nuevamente.
+                        El correo no existe, inente nuevamente.
                     </div>';
                 <?php endif; ?>
 
-                <!-- Mensaje de éxito al enviar correo -->
                 <?php if (isset($_REQUEST['success'])): ?>
-					<div class="alert alert-primary" role="alert">
-                        <strong>¡Felicidades!</strong> Revise su correo para continuar.
+                    <div class="alert alert-primary" role="alert">
+                        <strong>¡Felicidades</strong>
+                         Revise su correo para continuar.
                     </div>
                 <?php endif; ?>
+                <div id="rc">
+                    <a href="login.php">Iniciar sesion</a>
+                </div>
 
             </form>
         </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
 
